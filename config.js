e6f55ed6ed1e35b3c6f1f485efd3593a929805c5
Thekiet1 <86723971+Thekiet1@users.noreply.github.com>
2021-06-30 20:06:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 const CONFIG = {
-    introTitle: 'Baybe à!',
-    introDesc: 'Cậu á, đi không cẩn thận gì cả. Ngã vào tim tớ rồi đây này.Chính vì thế : My heart calls out for you.',
-    btnIntro: 'Ấn vô đây nè :3',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'Babe à!',
+    introDesc: `Cậu đi đứng kiểu gì mà ngã vào tim tui vậy. Bắt đền đó :))`,
+    btnIntro: 'Ấn vào đây để nhận hình phạt',
+    title: 'Hình phạt là phải YÊU TUI  😙',
+    desc: 'Chọn nhanh đi bạn ơi :))) ',
     btnYes: 'Yêu anh <33',
-    btnNo: 'Không, dell rảnh :3',
+    btnNo: 'Không, ai gảnh =))',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Mãi yêu anh 3000',
+    reply: 'Yêu anh 3000 <33',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
